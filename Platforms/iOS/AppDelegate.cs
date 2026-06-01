aee24707947302776f36ccd4b4a6e05fd080105a
Ziad Gad <ziadmohamedgad@gmail.com>
2026-06-01 21:11:34 +0300

    readme file edited, un important files deleted

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Investy.Mobile;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
